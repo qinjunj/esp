6d2b7519fa1880a5568560f2b9154dfdfc0915b0
Frithjof Gressmann <fg14@illinois.edu>
2022-05-02 23:46:12 -0500

    Add spandex baremetal boilerplate

--- accelerators/vivado_hls/rotateorder2_vivado/sw/baremetal/rotateorder2.c
+++ accelerators/vivado_hls/rotateorder2_vivado/sw/baremetal/rotateorder2.c
@@ -11,6 +11,25 @@
 #include <esp_probe.h>
 #include <fixed_point.h>
 
+#define SPANDEX_MODE 4
+
+typedef union
+{
+  struct
+  {
+    unsigned char r_en   : 1;
+    unsigned char r_op   : 1;
+    unsigned char r_type : 2;
+    unsigned char r_cid  : 4;
+    unsigned char w_en   : 1;
+    unsigned char w_op   : 1;
+    unsigned char w_type : 2;
+    unsigned char w_cid  : 4;
+	uint16_t reserved: 16;
+  };
+  uint32_t spandex_reg;
+} spandex_config_t;
+
 typedef float  token_t;
 
 static unsigned DMA_WORD_PER_BEAT(unsigned _st)
@@ -36,6 +55,18 @@ static inline uint64_t get_counter()
 	return counter;
 }
 
+void load_aq () {
+#if(SPANDEX_MODE == 2)
+	asm volatile ("fence r, r");
+#endif
+}
+
+void store_rl () {
+#if(SPANDEX_MODE > 1)
+	asm volatile ("fence w, w");
+#endif
+}
+
 static float my_abs(float a){
 	if(a > 0)
 		return a;
@@ -50,6 +81,11 @@ const int32_t nBatches = 1;
 const int32_t channels = 5;
 const int32_t nSamples = 1024;
 
+const int32_t LOG_LEN_VAR = 10; // todo: find value
+const int32_t log_len = LOG_LEN_VAR;
+int32_t len;
+int32_t do_bitrev = 1;
+
 static unsigned in_words_adj;
 static unsigned out_words_adj;
 static unsigned in_len;
@@ -59,8 +95,10 @@ static unsigned out_size;
 static unsigned out_offset;
 static unsigned mem_size;
 
-uint64_t start_cnt_pc;
-uint64_t end_cnt_pc;
+uint64_t start_cpu_cnt,end_cpu_cnt;
+uint64_t start_init_buf_cnt, end_init_buf_cnt;
+uint64_t start_val_buf_cnt, end_val_buf_cnt;
+int iter = 0;
 
 /* Size of the contiguous chunks for scatter/gather */
 #define CHUNK_SHIFT 20
@@ -76,16 +114,56 @@ uint64_t end_cnt_pc;
 #define ROTATEORDER2_NSAMPLES_REG 0x40
 
 
+
 static int validate_buf(token_t *out, token_t *gold)
 {
 	int i;
 	int j;
 	int errors = 0;
-
+	float value;
+	if (iter == 1) start_val_buf_cnt = get_counter();
 	for (i = 0; i < nBatches; i++)
-		for (j = 0; j < channels * nSamples; j++)
-			if (my_abs(gold[i * out_words_adj + j] - out[i * out_words_adj + j]) > 0.00001)
+		for (j = 0; j < channels * nSamples; j++) {
+			#if (SPANDEX_MODE == 2)
+					void* dst = (void*)((uint64_t)(out+ i * out_words_adj + j));
+
+					asm volatile (
+						"mv t0, %1;"
+						".word 0x2002B30B;"
+						"mv %0, t1"
+						: "=r" (value)
+						: "r" (dst)
+						: "t0", "t1", "memory"
+					);
+			#elif (SPANDEX_MODE > 2)
+					void* dst = (void*)((uint64_t)(out+j));
+
+					asm volatile (
+						"mv t0, %1;"
+						".word 0x4002B30B;"
+						"mv %0, t1"
+						: "=r" (value)
+						: "r" (dst)
+						: "t0", "t1", "memory"
+					);
+			#else
+					value = out[i * out_words_adj + j];
+					// void* dst = (void*)((uint64_t)(out+j));
+
+					// asm volatile (
+					// 	"mv t0, %1;"
+					// 	".word 0x4002B30B;"
+					// 	"mv %0, t1"
+					// 	: "=r" (value)
+					// 	: "r" (dst)
+					// 	: "t0", "t1", "memory"
+					// );
+			#endif
+					//if (value == 0xcafebeef) j = 0; // todo: not sure
+			if (my_abs(gold[i * out_words_adj + j] - value) > 0.00001)
 				errors++;
+		}
+	if (iter == 1) end_val_buf_cnt = get_counter();
 
     printf("errors in baremetal: %d\n", errors); 
 	return errors;
@@ -97,11 +175,51 @@ static void init_buf (token_t *in, token_t * gold)
 	int i;
 	int j;
 
+	if (iter == 1) start_init_buf_cnt = get_counter();
+
     for (i = 0; i < channels; i++) {
         for (j = 0; j < nSamples; j++) {
-            in[nSamples*i+j] = (token_t) (-1.0 + j/(float)(nSamples/2)); // random between -1 and 1 to mimic sin cos
+            token_t value = (token_t) (-1.0 + j/(float)(nSamples/2));  // random between -1 and 1 to mimic sin cos
+			#if (SPANDEX_MODE == 3)
+					void* dst = (void*)((uint64_t)(in+nSamples*i+j));
+
+					asm volatile (
+						"mv t0, %0;"
+						"mv t1, %1;"
+						".word 0x2062B02B"
+						: 
+						: "r" (dst), "r" (value)
+						: "t0", "t1", "memory"
+					);
+			#elif (SPANDEX_MODE == 4)
+					void* dst = (void*)((uint64_t)(in+nSamples*i+j));
+
+					asm volatile (
+						"mv t0, %0;"
+						"mv t1, %1;"
+						".word 0x2262B82B"
+						: 
+						: "r" (dst), "r" (value)
+						: "t0", "t1", "memory"
+					);
+			#else
+					in[nSamples*i+j] = value;
+					// void* dst = (void*)((uint64_t)(in+j));
+
+					// asm volatile (
+					// 	"mv t0, %0;"
+					// 	"mv t1, %1;"
+					// 	".word 0x2262B82B"
+					// 	: 
+					// 	: "r" (dst), "r" (value)
+					// 	: "t0", "t1", "memory"
+					// );
+			#endif
         }
     }
+
+	if (iter == 1) end_init_buf_cnt = get_counter();
+
     const float m_fCosAlpha = -4.37114e-08;
     const float m_fSinAlpha = -1;
     const float m_fCosBeta = -0.49908;
@@ -124,7 +242,12 @@ static void init_buf (token_t *in, token_t * gold)
     float m_pfTempSample[5] = {0};
     const float fSqrt3 = sqrt(3.f);
 
-    start_cnt_pc = get_counter(); 
+    if (iter == 1) start_cpu_cnt = get_counter();
+
+	int64_t value = 0;
+
+    int local_len = len; 
+
     for (int niSample = 0; niSample < nSamples; niSample++) {
         // Alpha rotation 
         m_pfTempSample[kV] = - in[kU*nSamples+niSample] * m_fSin2Alpha
@@ -170,7 +293,7 @@ static void init_buf (token_t *in, token_t * gold)
         gold[kV*nSamples+niSample] = m_pfTempSample[kV]; 
 
     }
-    end_cnt_pc = get_counter(); 
+    if (iter == 1) end_cpu_cnt = get_counter(); 
     /*
     FILE *inputFile = fopen("/scratch/projects/qinjunj2/esp/accelerators/vivado_hls/rotateorder2_vivado/hw/tb/inputs_2.txt", "r");
     FILE *outputFile = fopen("/scratch/projects/qinjunj2/esp/accelerators/vivado_hls/rotateorder2_vivado/hw/tb/outputs_2.txt", "r");
@@ -213,9 +336,16 @@ int main(int argc, char * argv[])
 	token_t *mem;
 	token_t *gold;
 	unsigned errors = 0;
-	unsigned coherence;
+	const int ERROR_COUNT_TH = 0.001;
+	#if (SPANDEX_MODE == 0)
+		unsigned coherence = ACC_COH_RECALL;
+	#else
+		unsigned coherence = ACC_COH_FULL;
+	#endif
     uint64_t start_cnt;
 	uint64_t end_cnt;
+
+	len = 1 << log_len;
 
 	if (DMA_WORD_PER_BEAT(sizeof(token_t)) == 0) {
 		in_words_adj = channels * nSamples;
@@ -234,6 +364,7 @@ int main(int argc, char * argv[])
 
 	// Search for the device
 	printf("Scanning device tree... \n");
+	printf("SPANDEX_MODE = %0d SIZE = %0d\n", SPANDEX_MODE, log_len);
 
 	ndev = probe(&espdevs, VENDOR_SLD, SLD_ROTATEORDER2, DEV_NAME);
 	if (ndev == 0) {
@@ -281,7 +412,7 @@ int main(int argc, char * argv[])
             // enum accelerator_coherence {ACC_COH_NONE = 0, ACC_COH_LLC, ACC_COH_RECALL, ACC_COH_FULL, ACC_COH_AUTO};
 			printf("  --------------------\n");
 			printf("  Generate input...\n");
-			init_buf(mem, gold);
+			
 
 			// Pass common configuration parameters
 
@@ -305,15 +436,45 @@ int main(int argc, char * argv[])
 		iowrite32(dev, ROTATEORDER2_NBATCHES_REG, nBatches);
 		iowrite32(dev, ROTATEORDER2_CHANNELS_REG, channels);
 		iowrite32(dev, ROTATEORDER2_NSAMPLES_REG, nSamples);
+
+		#if (SPANDEX_MODE > 1)
+				spandex_config_t spandex_config;
+				spandex_config.spandex_reg = 0;
+		#if (SPANDEX_MODE == 2)
+				spandex_config.r_en = 1;
+				spandex_config.r_type = 1;
+		#elif (SPANDEX_MODE == 3)
+				spandex_config.r_en = 1;
+				spandex_config.r_type = 2;
+				spandex_config.w_en = 1;
+				spandex_config.w_type = 1;
+		#elif (SPANDEX_MODE == 4)
+				spandex_config.r_en = 1;
+				spandex_config.r_type = 2;
+				spandex_config.w_en = 1;
+				spandex_config.w_op = 1;
+				spandex_config.w_type = 1;
+		#endif
+				iowrite32(dev, SPANDEX_REG, spandex_config.spandex_reg);
+		#endif
+
+		for (iter = 0; iter < 2; iter++)
+		{
+
+			init_buf(mem, gold);
+
+			store_rl();
             
-            printf("  Start...\n");
-            start_cnt = get_counter(); 
+            //printf("  Start...\n");
+            if (iter == 1) start_cnt = get_counter(); 
 			// Flush (customize coherence model here)
-			esp_flush(coherence);
+			//esp_flush(coherence);
 
 			// Start accelerators
 			// printf("  Start...\n");
 			iowrite32(dev, CMD_REG, CMD_MASK_START);
+
+			load_aq();
 
 			// Wait for completion
 			done = 0;
@@ -322,22 +483,35 @@ int main(int argc, char * argv[])
 				done &= STATUS_MASK_DONE;
                 printf("not done...\n");  
 			}
+
+			if (iter == 1) end_cnt = get_counter(); 
+
 			iowrite32(dev, CMD_REG, 0x0);
-            end_cnt = get_counter(); 
+            
 
 			printf("  Done\n");
 			printf("  validating...\n");
 
 			/* Validation */
 			errors = validate_buf(&mem[out_offset], gold);
-			if (errors)
-				printf("  ... FAIL\n");
-			else
-				printf("  ... PASS\n");
+			printf ("  ... Errors = %d\n", errors);
+			
 		}
+
+		} // coherence protocol for
         
-        printf("acc counter: %lu\n", end_cnt-start_cnt);
-		printf("cpu  counter: %lu\n", end_cnt_pc-start_cnt_pc);
+		printf("CPU baseline:\n");
+		printf("cpu time: %lu\n\n", end_cpu_cnt-start_cpu_cnt);
+		printf("Time for accelerator without CPU I/O:\n");
+		printf("acc time: %lu\n\n", end_cnt-start_cnt);
+		printf("Time for CPU phase:\n");
+		printf("init_buf time: %lu\n", end_init_buf_cnt-start_init_buf_cnt);
+		printf("val_buf time: %lu\n\n", end_val_buf_cnt-start_val_buf_cnt);
+
+		if ((errors / len) > ERROR_COUNT_TH)
+			printf("  ... FAIL\n");
+		else
+			printf("  ... PASS\n");
         
 		aligned_free(ptable);
 		aligned_free(mem);
